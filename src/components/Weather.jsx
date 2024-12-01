71ab67c10c45570dcec3b4775cc3acd90bb48654
Fudail Mohammed Zafar <fudail.zafar@gmail.com>
2024-12-01 22:08:12 +0530

    file name change

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./Weather.css";
+import "./Weather2.css";
 import search_icon from "../assets/search.png";
 import clear_icon from "../assets/clear.png";
 import cloud_icon from "../assets/cloud.png";
@@ -9,7 +9,7 @@ import rain_icon from "../assets/rain.png";
 import snow_icon from "../assets/snow.png";
 import wind_icon from "../assets/wind.png";
 
-const Weather = () => {
+const weather = () => {
   const inputRef = useRef();
   const [weatherData, setWeatherData] = useState(false);
 
@@ -106,4 +106,4 @@ const Weather = () => {
   );
 };
 
-export default Weather;
+export default weather;
